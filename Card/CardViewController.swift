82cabe6558bec0f1527e9005f348f86a41ac8874
Kondol <kyk9702@gmail.com>
2025-05-23 23:33:05 +0900

    init - 카드 스토리보드, 기본 기능(숫자비교)
    숫자 같으면 카드 사라짐, 다르면 다시 뒤집힘

---
+++ Card/CardViewController.swift
@@ -0,0 +1,135 @@
+//
+//  ViewController.swift
+//  Card
+//
+//  Created by yk on 5/16/25.
+//
+
+import UIKit
+
+class CardViewController: UIViewController {
+    
+    @IBOutlet weak var cardCollectionView: UICollectionView!
+    
+    var firstSelectedIndexPath: IndexPath? // 첫번째 선택 셀 위치
+    var secondSelectedIndexPath: IndexPath? // 두번째 선택 셀 위치
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    func checkForMatch() {
+        guard let firstIndex = firstSelectedIndexPath,
+              let secondIndex = secondSelectedIndexPath,
+              let firstCell = cardCollectionView.cellForItem(at: firstIndex) as? CardCollectionViewCell,
+              let secondCell = cardCollectionView.cellForItem(at: secondIndex) as? CardCollectionViewCell
+        else { return }
+        
+        let firstValue = firstCell.frontLabel.text
+        let secondValue = secondCell.frontLabel.text
+        
+        if firstValue == secondValue {
+            // 정답 0.5초 뒤에 사라짐
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                firstCell.isHidden = true
+                secondCell.isHidden = true
+            }
+        } else {
+            // 오답 1 초 뒤에 다시 뒤집힘
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                firstCell.flip()
+                secondCell.flip()
+            }
+        }
+        
+        // 선택초기화
+        firstSelectedIndexPath = nil
+        secondSelectedIndexPath = nil
+    }
+}
+
+
+
+extension CardViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        guard let cell = collectionView.cellForItem(at: indexPath) as? CardCollectionViewCell else { return }
+        
+        // 이미 선택한 셀 다시 클릭 못하게
+        if indexPath == firstSelectedIndexPath { return }
+        
+        // 카드 앞면 보이게 뒤집기
+        cell.flip()
+        
+        // 첫번째 선택이면
+        if firstSelectedIndexPath == nil {
+            firstSelectedIndexPath = indexPath
+        }
+        else if secondSelectedIndexPath == nil {
+            secondSelectedIndexPath = indexPath
+            
+            // 두개 다 선택 됬으면 비교
+            checkForMatch()
+        }
+    }
+}
+
+
+extension CardViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: String(describing: CardCollectionViewCell.self), for: indexPath) as! CardCollectionViewCell
+        
+        cell.frontLabel.text = [indexPath.row + 1].map(String.init).joined()
+        
+        
+        return cell
+    }
+    
+}
+
+
+extension CardViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        
+        guard let flowLayout = collectionViewLayout as? UICollectionViewFlowLayout else {
+            return .zero
+        }
+        
+        // Int로 하는이유 - 소수점오차로 UI 망가질까봐
+        let width = (collectionView.bounds.width - flowLayout.minimumLineSpacing + flowLayout.sectionInset.left + flowLayout.sectionInset.right) / 6
+        
+        let height = width * 1.4
+        
+        return CGSize(width: width, height: height)
+    }
+    
+    
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 8)
+    }
+    
+    // 최소 라인여백
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 8
+    }
+    
+    // 최소 셀여백
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 8
+    }
+}
---
+++ Card/Cell/CardCollectionViewCell.swift
@@ -0,0 +1,61 @@
+//
+//  CardCollectionViewCell.swift
+//  Card
+//
+//  Created by yk on 5/17/25.
+//
+
+import UIKit
+
+class CardCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var cardContainerView: UIView!
+    @IBOutlet weak var backView: UIView!
+    @IBOutlet weak var frontView: UIView!
+    
+    
+    @IBOutlet weak var backImageView: UIImageView!
+    @IBOutlet weak var frontLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        frontLabel.font = UIFont.monospacedDigitSystemFont(ofSize: 30, weight: .bold)
+        
+        self.layer.borderColor = UIColor.black.cgColor
+        self.layer.borderWidth = 2
+        
+        self.layer.cornerRadius = 8.0
+        self.layer.masksToBounds = true
+        
+        
+        
+        frontView.isHidden = true
+        backView.isHidden = false
+        isFlipped = true
+    }
+    
+    // 셀 재사용 될때도 상태 초기화
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        frontView.isHidden = true
+        backView.isHidden = false
+        isFlipped = true
+    }
+    
+    var isFlipped = false
+    
+    func flip() {
+        let fromView = isFlipped ? backView : frontView // 현재 화면에 보이는뷰
+        let toView = isFlipped ? frontView : backView   // 나중에 보일 뷰
+        
+        UIView.transition(from: fromView!,
+                          to: toView!,
+                          duration: 0.4,
+                          options: [
+                            .transitionFlipFromLeft, .showHideTransitionViews]) { [weak self] _ in
+                                self?.isFlipped.toggle()
+                            }
+        }
+        
+}
